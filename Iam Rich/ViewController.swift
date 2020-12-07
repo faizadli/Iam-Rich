d8e42c29547ed43c0eeff4a6740d0d868df47d07
faizadli <68326914+faizadli@users.noreply.github.com>
2020-12-07 14:55:11 +0700

    Add files via upload

---
+++ Iam Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iam Rich
+//
+//  Created by faiz on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
